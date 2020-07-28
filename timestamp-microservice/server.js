6dc551c55bf8d3b0f775c1cb28722357f8c9954a
chidibede <chidibede@gmail.com>
2020-07-28 12:54:00 +0100

    Built the api endpoint, incase no parameter is input

--- timestamp-microservice/server.js
+++ timestamp-microservice/server.js
@@ -2,31 +2,45 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
+var getUtcDate = require("./functions");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// API endpoint with timestamp parameter...
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let date_string = req.params.date_string;
+  date_string = date_string.replace(/-/g, ".");
+  console.log(date_string);
+  res.json({ unix: date_string, utc: date_string });
 });
 
-
+// API endpoint Incase no parameter is passed
+app.get("/api/timestamp", (req, res) => {
+  dateObj = new Date();
+  let utc_date = getUtcDate(dateObj);
+  let unix_date = parseInt((dateObj.getTime() / 1000).toFixed(0));
+  res.json({
+    unix: unix_date,
+    utc: utc_date,
+  });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+let env_port = process.env.PORT;
+let local_port = 3000;
+var listener = app.listen(local_port || env_port, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
